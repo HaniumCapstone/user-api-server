862175feaf5cef37a7ad4d9a0fdff39f48d55c41
이본영 <5thofnovmbr@gmail.com>
2023-07-26 22:57:03 +0900

    test: 테스트 파일 의존성 주입

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,6 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+
+const mockRepository = () => ({
+  join: jest.fn(),
+  login: jest.fn(),
+  logout: jest.fn(),
+  users: jest.fn(),
+  profile: jest.fn(),
+  createMBTI: jest.fn(),
+  updateMBTI: jest.fn()
+})
 
 describe('UserController', () => {
   let controller: UserController;
@@ -8,7 +20,11 @@ describe('UserController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [UserService],
+      providers: [UserService,
+        {
+          provide: getRepositoryToken(User), useValue: mockRepository()
+        }
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,12 +1,27 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test, TestingModule, } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { User } from './entities/user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+
+const mockRepository = () => ({
+  join: jest.fn(),
+  login: jest.fn(),
+  logout: jest.fn(),
+  users: jest.fn(),
+  profile: jest.fn(),
+  createMBTI: jest.fn(),
+  updateMBTI: jest.fn()
+})
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [UserService, {
+        provide: getRepositoryToken(User), useValue: mockRepository()
+      }],
     }).compile();
 
     service = module.get<UserService>(UserService);
